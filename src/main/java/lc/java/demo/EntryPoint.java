a46352b7667e14f1f5808113923a5cba15adf60b
mansub.walke <mansub.walke@gmail.com>
2019-12-04 12:29:44 +0530

    sdf

---
+++ src/main/java/lc/java/demo/EntryPoint.java
@@ -0,0 +1,12 @@
+package lc.java.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class EntryPoint {
+
+    private static final Logger LOG =LoggerFactory.getLogger(EntryPoint.class.getName());
+    public static void main (String [] args)
+    {
+        LOG.info("Hello");
+    }
+}
